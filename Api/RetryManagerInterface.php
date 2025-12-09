1cb4b73b1dd2f160fc1e75474ef2ee5ff4c5514e
Sandip Chandela <49310430+sandipklevu@users.noreply.github.com>
2025-12-09 20:01:42 +0530

    - Update Handler (#24)
    - Delete Handler
    - Max request mechanism
    - Logging in processor for each operation

---
+++ Api/RetryManagerInterface.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright (C) 2025 AthosCommerce <https://athoscommerce.com>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace AthosCommerce\Feed\Api;
+
+interface RetryManagerInterface
+{
+    /**
+     * @param int $entityId
+     * @param string $action
+     * @param string|null $errorMessage
+     *
+     * @return void
+     */
+    public function markForRetry(int $entityId, string $action, ?string $errorMessage = null): void;
+
+    /**
+     * @param int $entityId
+     * @param string $action
+     *
+     * @return void
+     */
+    public function resetRetry(int $entityId, string $action): void;
+}
